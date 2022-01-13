a992fff12796090b897946290ddfec8e153ce79a
Listen-Y <1604053140@qq.com>
2022-01-13 23:59:56 +0800

    初始化服务器

---
+++ server/src/main/java/com/faculty_research_info_mis/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.faculty_research_info_mis.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/faculty_research_info_mis/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faculty_research_info_mis.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
