3ababea409465ae99f5f1a99be23177d4022dea1
Listen-Y <1604053140@qq.com>
2022-01-28 16:48:50 +0800

    分页插件开发

---
+++ server/src/main/java/com/faculty_research_info_mis/server/config/MybatisPlusConfig.java
@@ -0,0 +1,33 @@
+package com.faculty_research_info_mis.server.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 分页插件配置
+ * Created with IntelliJ IDEA.
+ * Description: If you don't work hard, you will be a loser.
+ * User: Listen-Y.
+ * Date: 2022-01-28
+ * Time: 16:45
+ */
+
+@Configuration
+@MapperScan("com.faculty_research_info_mis.server.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
