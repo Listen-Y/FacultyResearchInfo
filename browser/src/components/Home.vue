dd7056f0d7eecc32041d4565a3d9535172842dda
Listen-Y <1604053140@qq.com>
2022-02-09 18:34:05 +0800

    开发home页侧栏

---
+++ browser/src/components/Home.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <!--    头部-->
+        <Header />
+
+        <!--    主体-->
+        <div style="display: flex">
+            <!--      侧边栏-->
+            <Aside />
+            <!--      内容区域-->
+            <router-view style="flex: 1" @userInfo="refreshUser"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from "@/components/Header";
+    import Aside from "@/components/Aside";
+    import request from "@/utils/request";
+
+    export default {
+        name: "Home",
+        components: {
+            Header,
+            Aside
+        },
+        data() {
+            return {
+                user: {}
+            }
+        },
+        created() {
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
