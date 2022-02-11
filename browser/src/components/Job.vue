741507ca7f07fb5a127914a33497a226ec43c326
Listen-Y <1604053140@qq.com>
2022-02-11 18:32:42 +0800

    开发job前端的增加和查找

--- browser/src/components/Job.vue
+++ browser/src/components/Job.vue
@@ -14,7 +14,7 @@
                     clearable
             >
             </el-cascader>
-            <el-button type="primary" style="margin-left: 5px" @click="loadLike">查询</el-button>
+            <el-button type="primary" style="margin-left: 5px" @click="loadLikeByJob">查询</el-button>
         </div>
         <el-table
                 v-loading="loading"
@@ -41,11 +41,6 @@
             </el-table-column>
             <el-table-column
                     width="180"
-                    prop="sex"
-                    label="性别">
-            </el-table-column>
-            <el-table-column
-                    width="180"
                     prop="phone"
                     label="联系电话">
             </el-table-column>
@@ -56,7 +51,8 @@
             </el-table-column>
             <el-table-column label="操作">
                 <template #default="scope">
-                    <el-button size="mini" type="primary" @click="getFullInfo(scope.row)">查看其专业技术证明</el-button>
+                    <el-button size="mini" type="success" plain @click="getFullInfo(scope.row.id)">查看其专业技术证明</el-button>
+                    <el-button size="mini" type="primary" plain @click="addBefore(scope.row.id)">添加专业技术证明</el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -72,6 +68,92 @@
                     :total="total">
             </el-pagination>
         </div>
+
+        <el-dialog title="专业技术条目" @close="jobData = []" v-model="dialogVisible" width="90%">
+            <el-table :data="jobData"
+                      border
+                      stripe
+            >
+                <el-table-column prop="qualificationName" label="任职资格名称" width="110px" ></el-table-column>
+                <el-table-column prop="accessQualification" label="取得资格途径" width="110px" ></el-table-column>
+                <el-table-column prop="reviewUnit" label="评审单位" width="110px"></el-table-column>
+                <el-table-column prop="evaluationDate" label="评定日期" width="110px" ></el-table-column>
+                <el-table-column prop="appointment" label="聘任职务" width="110px"></el-table-column>
+                <el-table-column prop="employingUnit" label="聘任单位" width="110px" ></el-table-column>
+                <el-table-column prop="dateAppointment" label="聘任日期" width="110px" ></el-table-column>
+                <el-table-column prop="employment" label="聘任情况" width="110px" ></el-table-column>
+                <el-table-column prop="dateDismissal" label="解聘日期" width="110px"></el-table-column>
+                <el-table-column prop="reasonDismissal" label="解聘原因" width="110px"></el-table-column>
+                <el-table-column label="操作">
+                    <template #default="scope">
+                        <el-button size="mini" type="success" @click="handleDelete(scope.row)">编辑</el-button>
+                        <el-popconfirm title="确定删除吗？" @confirm="handleDelete(scope.row.id)">
+                            <template #reference>
+                                <el-button size="mini" type="danger">删除</el-button>
+                            </template>
+                        </el-popconfirm>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-dialog>
+
+        <!--add-->
+        <el-dialog title="添加专业技术职务条目" v-model="dialogVisibleAdd" width="50%" :before-close="handleClose">
+            <el-form :model="form" :rules="rules">
+                <el-form-item label="任职资格名称" prop="qualificationName">
+                    <el-cascader
+                            placeholder="请选择任职资格名称"
+                            :options="options"
+                            filterable
+                            clearable
+                            v-model="form.qualificationName"
+                    >
+                    </el-cascader>
+                </el-form-item>
+                <el-form-item label="取得资格途径" prop="accessQualification">
+                    <el-select v-model="form.accessQualification" placeholder="请选择活动区域">
+                        <el-option label="评审" value="shanghai"></el-option>
+                        <el-option label="考试" value="beijing"></el-option>
+                        <el-option label="其他" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="评审单位" prop="reviewUnit">
+                    <el-input v-model="form.reviewUnit" style="width: 80%"></el-input>
+                </el-form-item>
+                <el-form-item label="评定日期" prop="evaluationDate">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.evaluationDate" style="width: 50%;"></el-date-picker>
+                </el-form-item>
+                <el-form-item label="聘任职务" prop="appointment">
+                    <el-input v-model="form.appointment" style="width: 80%"></el-input>
+                </el-form-item>
+                <el-form-item label="聘任单位" prop="employingUnit">
+                    <el-input v-model="form.employingUnit" style="width: 80%"></el-input>
+                </el-form-item>
+                <el-form-item label="聘任日期" prop="dateAppointment">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.dateAppointment" style="width: 50%;"></el-date-picker>
+                </el-form-item>
+                <el-form-item label="聘任情况" prop="employment">
+                    <el-select v-model="form.employment" placeholder="请选择活动区域">
+                        <el-option label="已聘" value="已聘"></el-option>
+                        <el-option label="高聘" value="高聘"></el-option>
+                        <el-option label="低聘" value="低聘"></el-option>
+                        <el-option label="待聘" value="待聘"></el-option>
+                        <el-option label="未聘" value="未聘"></el-option>
+                        <el-option label="其他" value="其他"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="解聘日期" prop="dateDismissal">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.dateDismissal" style="width: 50%;"></el-date-picker>
+                </el-form-item>
+                <el-form-item label="解聘原因" prop="reasonDismissal">
+                    <el-input v-model="form.reasonDismissal" style="width: 80%"></el-input>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogVisibleAdd = false">取 消</el-button>
+                <el-button type="primary" @click="add()">确 定</el-button>
+            </div>
+        </el-dialog>
 
     </div>
 </template>
@@ -89,11 +171,14 @@
                 form: {},
                 dialogVisible: false,
                 dialogVisibleReadOnly: false,
+                dialogVisibleAdd: false,
+                addTeacherId: -1,
                 search: '',
                 currentPage: 1,
                 pageSize: 10,
                 total: 0,
                 tableData: [],
+                jobData: [],
                 fullInfo: {},
                 options: [{
                     value: 'zhinan',
@@ -291,8 +376,35 @@
                     }]
                 }],
                 rules: {
-                    teacherId: [
+                    qualificationName: [
                         { required: true, message: '请输入', trigger: 'blur' },
+                    ],
+                    accessQualification: [
+                        { required: true, message: '请输入', trigger: 'blur' },
+                    ],
+                    reviewUnit: [
+                        { required: true, message: '请输入', trigger: 'blur' },
+                    ],
+                    evaluationDate: [
+                        { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+                    ],
+                    appointment: [
+                        { required: true, message: '请输入', trigger: 'blur' },
+                    ],
+                    employingUnit: [
+                        { required: true, message: '请输入', trigger: 'blur' },
+                    ],
+                    dateAppointment: [
+                        { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+                    ],
+                    employment: [
+                        { required: true, message: '请输入', trigger: 'blur' },
+                    ],
+                    dateDismissal: [
+                        { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+                    ],
+                    reasonDismissal: [
+
                     ],
                 },
             }
@@ -301,6 +413,9 @@
             this.load()
         },
         methods: {
+            loadLikeByJob() {
+
+            },
             loadLike() {
                 this.loading = true
                 request.get("/teacher/like", {
@@ -329,11 +444,36 @@
                 })
             },
             add() {
-                this.dialogVisible = true
-                this.form = {}
+                const loadDate = {
+                    qualificationName: this.form.qualificationName[2],
+                    accessQualification: this.form.accessQualification,
+                    reviewUnit: this.form.reviewUnit,
+                    evaluationDate: this.form.evaluationDate,
+                    appointment: this.form.appointment,
+                    employingUnit: this.form.employingUnit,
+                    dateAppointment: this.form.dateAppointment,
+                    employment: this.form.employment,
+                    dateDismissal: this.form.dateDismissal,
+                    reasonDismissal: this.form.reasonDismissal,
+                    teacherId: this.addTeacherId
+                };
+                request.post("/job/add", loadDate).then(res => {
+                    if (res.code === '0') {
+                        this.$message({
+                            type: "success",
+                            message: "添加成功"
+                        })
+                    } else {
+                        this.$message({
+                            type: "error",
+                            message: res.msg
+                        })
+                    }
+                })
+                    this.addTeacherId = -1
+                    this.dialogVisibleAdd = false
             },
             save() {
-
                 if (this.form.id) {  // 更新
                     request.post("/teacher/update", this.form).then(res => {
                         console.log(res)
@@ -372,13 +512,23 @@
                 }
 
             },
-            handleEdit(row) {
-                this.form = JSON.parse(JSON.stringify(row))
+            getFullInfo(id) {
+                request.get("/job/teacher_id/" + id, {
+                    params: {
+                        pageNum: this.currentPage,
+                        pageSize: this.pageSize,
+                    }
+                }).then(res => {
+                    this.jobData = res.data.records
+                    this.total = res.data.total
+                })
                 this.dialogVisible = true
             },
-            getFullInfo(row) {
-                this.form = JSON.parse(JSON.stringify(row))
-                this.dialogVisibleReadOnly = true
+            addBefore(id) {
+                this.addTeacherId = id
+                console.log(this.addTeacherId)
+                this.form = {}
+                this.dialogVisibleAdd = true
             },
             handleDelete(id) {
                 console.log(id)
@@ -404,6 +554,13 @@
             handleCurrentChange(pageNum) {  // 改变当前页码触发
                 this.currentPage = pageNum
                 this.load()
+            },
+            handleClose(done) {
+                this.$confirm('确认关闭？')
+                    .then(_ => {
+                        done();
+                    })
+                    .catch(_ => {});
             }
         }
     }
