0fcf1a5b1614b221d60da44afb21716d72f5bc1d
Listen-Y <1604053140@qq.com>
2022-02-19 23:39:37 +0800

    开发user的管理

--- server/src/main/java/com/faculty_research_info_mis/server/controller/UserController.java
+++ server/src/main/java/com/faculty_research_info_mis/server/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.faculty_research_info_mis.server.controller;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.log.Log;
 import cn.hutool.log.LogFactory;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
@@ -10,11 +11,15 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.faculty_research_info_mis.server.component.Result;
 import com.faculty_research_info_mis.server.model.TeacherBasicInfo;
+import com.faculty_research_info_mis.server.model.TreatiseBasicInfo;
 import com.faculty_research_info_mis.server.model.User;
 import com.faculty_research_info_mis.server.service.UserService;
 import com.faculty_research_info_mis.server.util.TokenUtil;
+import lombok.Data;
 import lombok.val;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
 
 /**
  * 用户账号处理
@@ -66,6 +71,12 @@ public class UserController {
 
         // 生成token
         String token = TokenUtil.genToken(res);
+
+        // 统计登录次数和最后一次登录时间
+
+        res.setTimes(res.getTimes() + 1);
+        res.setLastLoginTime(new Date(System.currentTimeMillis()));
+        userService.userMapper.updateById(res);
         return Result.success(token);
     }
 
@@ -88,7 +99,39 @@ public class UserController {
         userInfo.setUserName(user.getUserName());
         userInfo.setPassword(user.getPassword());
         userInfo.setAdmin(0);
+        userInfo.setTimes(0);
+        userInfo.setLastLoginTime(new Date(System.currentTimeMillis()));
+        userInfo.setCreateTime(new Date(System.currentTimeMillis()));
         userService.userMapper.insert(userInfo);
+
+        return Result.success();
+    }
+
+    @Data
+    static class ChangePassWard {
+        private String userName;
+        private String oldPassword;
+        private String newPassword;
+    }
+
+    @PostMapping("/change")
+    public Result<?> changPassword(@RequestBody ChangePassWard changePassWard) {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("user_name", changePassWard.getUserName());
+        queryWrapper.eq("password", changePassWard.getOldPassword());
+        User res = userService.userMapper.selectOne(queryWrapper);
+
+        // 判断密码是否正确
+        if (res == null) {
+            return Result.error("9999", "旧密码错误");
+        }
+        if (changePassWard.oldPassword.equals(changePassWard.newPassword)) {
+            return Result.error("9999", "新密码不可与旧密码一致");
+        }
+
+        // 修改密码
+        res.setPassword(changePassWard.newPassword);
+        userService.userMapper.updateById(res);
 
         return Result.success();
     }
@@ -102,7 +145,13 @@ public class UserController {
     public Result<?> changePower(@PathVariable Integer id) {
         log.info("change power param: " + id);
         User tokenUser = TokenUtil.getUser();
-        if (tokenUser == null || tokenUser.getAdmin() != 1) {
+        if (tokenUser == null) {
+            return Result.error("9999", "token有误");
+        }
+        if (tokenUser.getId().equals(id)) {
+            return Result.error("9999", "自己怎么能修改自己嘞~");
+        }
+        if (tokenUser.getAdmin() != 1) {
             return Result.error("9999", "当前登录账号不能修改权限");
         }
         User user = userService.userMapper.selectById(id);
@@ -123,7 +172,7 @@ public class UserController {
 
         User tokenUser = TokenUtil.getUser();
         if (tokenUser == null || tokenUser.getAdmin() != 1) {
-            return Result.error("9999", "当前登录账号不能修改权限");
+            return Result.error("9999", "当前登录账号不能修改删除用户");
         }
         userService.userMapper.deleteById(id);
         return Result.success();
@@ -137,10 +186,34 @@ public class UserController {
     public Result<?> findAll(@RequestParam(defaultValue = "1") Integer pageNum,
                              @RequestParam(defaultValue = "10") Integer pageSize) {
 
+        User tokenUser = TokenUtil.getUser();
+        if (tokenUser == null || tokenUser.getAdmin() != 1) {
+            return Result.error("9999", "当前登录账号不能获取用户数据");
+        }
+
         log.info("get user page param: " + pageNum + " " + pageSize);
 
         LambdaQueryWrapper<User> wrapper = Wrappers.lambdaQuery();
         Page<User> selectPage = userService.userMapper.selectPage(new Page<>(pageNum, pageSize), wrapper);
         return Result.success(selectPage);
+    }
+
+    @GetMapping("/like")
+    private Result<?> findLike(@RequestParam(defaultValue = "1") Integer pageNum,
+                               @RequestParam(defaultValue = "10") Integer pageSize,
+                               @RequestParam(defaultValue = "") String search) {
+
+        User tokenUser = TokenUtil.getUser();
+        if (tokenUser == null || tokenUser.getAdmin() != 1) {
+            return Result.error("9999", "当前登录账号不能获取用户数据");
+        }
+
+        LambdaQueryWrapper<User> wrapper = Wrappers.lambdaQuery();
+        if (StrUtil.isNotBlank(search)) {
+            wrapper.like(User::getUserName, search);
+        }
+
+        Page<User> page = userService.userMapper.selectPage(new Page<>(pageNum, pageSize), wrapper);
+        return Result.success(page);
     }
 }
--- server/src/main/java/com/faculty_research_info_mis/server/model/User.java
+++ server/src/main/java/com/faculty_research_info_mis/server/model/User.java
@@ -3,6 +3,9 @@ package com.faculty_research_info_mis.server.model;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
+import sun.nio.cs.ext.IBM037;
+
+import java.sql.Date;
 
 /**
  * 用户表
@@ -35,4 +38,19 @@ public class User {
      * 是否为管理员 1是
      */
     private Integer admin;
+
+    /**
+     * 登录次数
+     */
+    private Integer times;
+
+    /**
+     * 最后一次登录时间
+     */
+    private Date lastLoginTime;
+
+    /**
+     * 注册时间
+     */
+    private Date createTime;
 }
